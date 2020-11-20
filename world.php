406404b2ea16c458c28aebdc3caef566c82dbf47
Ronaldo Willie <ronaldo_kc@outlook.com>
2020-11-19 19:31:54 -0500

    updated world.php

--- world.php
+++ world.php
@@ -1,17 +1,32 @@
 <?php
+$country = $_GET['country'];
+$all = $_GET['all'];
 $host = 'localhost';
 $username = 'lab5_user';
 $password = '';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+if ($all == true && (isset($all) || !empty($all))) // Checks if the parameters have been set
+{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
+if (isset($country) || !empty($country)) 
+{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
